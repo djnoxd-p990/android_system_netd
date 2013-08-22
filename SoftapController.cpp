8ca206f9f27a16e4c7146405a714ee945736dfa9
DJNoXD <randolf.kastenmaier@gmail.com>
2013-08-22 09:04:10 +0200

    Revert "A rough cleanup of netd/softap"
    This reverts commit 25753d500d9ef6ee04dad43ffa27287a6dc959ac.
    
    Conflicts:
    
    	SoftapController.cpp
    	SoftapController.h
    
    Change-Id: Ia7c90c42c3926a2a26137e6af8fe59f54f2aad52

--- SoftapController.cpp
+++ SoftapController.cpp
@@ -305,7 +305,8 @@ int SoftapController::setSoftap(int argc, char *argv[]) {
     }
 
     asprintf(&wbuf, "interface=%s\ndriver=" HOSTAPD_DRIVER_NAME "\nctrl_interface="
-            "/data/misc/wifi/hostapd\nssid=%s\nchannel=6\nieee80211n=1\n",
+            "/data/misc/wifi/hostapd\nssid=%s\nchannel=6\nieee80211n=1\n"
+            "hw_mode=g\n",
             iface, ssid);
 
     if (argc > 5) {
@@ -353,6 +354,7 @@ int SoftapController::setSoftap(int argc, char *argv[]) {
         return -1;
     }
 
+    close(fd);
 #else
     /* Create command line */
     i = addParam(i, "ASCII_CMD", "AP_CFG");
